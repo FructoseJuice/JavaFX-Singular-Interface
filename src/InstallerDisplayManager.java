50bb2642383b3f41481af41a0957b1e48ba56e0a
FructoseJuice <116052103+FructoseJuice@users.noreply.github.com>
2024-10-19 17:51:21 -0600

    Added JLatexMath library

--- src/InstallerDisplayManager.java
+++ src/InstallerDisplayManager.java
@@ -83,7 +83,7 @@ public class InstallerDisplayManager {
         DirectoryChooser directoryChooser = new DirectoryChooser();
 
         directoryChooser.setTitle("Choose MCGB Directory");
-        Director
+
     }
 
 
